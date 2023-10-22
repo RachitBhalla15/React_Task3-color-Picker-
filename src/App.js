43a9feaa60818f91cd21ea5a4cee13943686298a
RachitBhalla15 <rachitbhalla91@gmail.com>
2023-10-22 19:48:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import ColorPicker from './Components/ColorPicker';
+import './App.css'
+
+function App() {
+  const colors = [
+    '#FF0000',
+    '#00FF00',
+    '#0000FF',
+    '#FFFF00',
+    '#FF00FF',
+    '#00FFFF',
+    '#FFA500',
+    '#800080',
+    '#FFC0CB',
+    '#008000',
+    '#FF6347',
+    '#00CED1',
+    '#8B4513',
+    '#FF8C00',
+    '#4682B4',
+    '#FFD700',
+    '#8A2BE2', 
+    '#00FA9A', 
+    '#87CEEB', 
+    '#20B2AA', 
+    '#BA55D3', 
+    '#9932CC', 
+    '#B0E0E6', 
+  ];
+  
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const handleColorSelect = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="color-picker-title" >Color  Picker</h1>
+      <div className="app_colorpicker_container" style={{backgroundColor: selectedColor}}>
+        
+        <ColorPicker colors={colors} onColorSelect={handleColorSelect} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
